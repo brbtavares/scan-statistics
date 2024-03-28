9ccb5d560032a3aea03a739b276d98a8d6c36703
brbtavares <brbtavares@outlook.com>
2024-03-28 01:39:11 -0300

    add `vega-lite` functionality and plot the first output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use vega_lite_5::*;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+
+    let builder = thread::Builder::new().stack_size(10 * 1024 * 1024); // 10MB stack size
+    let handler = builder.spawn(|| {
+        let spec = r##"{
+            "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+            "data": {
+              "values": [
+                {
+                  "x": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+                  "y": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+                }
+              ]
+            },
+            "transform": [
+              {"flatten": ["x"]},
+              {"flatten": ["y"]},
+              {"calculate": "datum.x + ',' + datum.y", "as": "l"}
+            ],
+            "height": {"step": 32},
+            "width": {"step": 40},
+            "mark": {"type": "point", "size": 1600},
+            "encoding": {
+              "x": {"field": "x", "type": "ordinal", "axis": null},
+              "y": {"field": "y", "type": "ordinal", "axis": null},
+              "shape": {
+                "condition": {
+                  "test": "datum.y % 2 == 1",
+                  "value": "'M0,-0.6 L1,-1 L2,-0.6 L2,0.6 L1,1 L0,0.6 Z'"
+                },
+                "value": "'M-1,-0.6 L0,-1 L1,-0.6 L1,0.6 L0,1 Z'"
+              }
+            },
+            "config": {"view": {"stroke": null}}
+        }"##;
+    
+        let chart: Result<Vegalite, serde_json::Error> = serde_json::from_str(spec);
+        match chart {
+            Ok(chart) => {
+                let _ = chart.show();
+            }
+            Err(err) => {
+                eprintln!("Error: {}", err);
+            }
+        }
+    }).unwrap();
+
+    handler.join().unwrap();
+
 }
