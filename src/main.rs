291e9fd6ff34becac1edf2c159bf559419ec1a17
brbtavares <brbtavares@outlook.com>
2024-04-03 09:01:07 -0300

    delete `plot` functionality

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 mod definitions;
 use crate::definitions::*;
-use std::thread;
-use vega_lite_5::*;
-use serde_json;
 
 fn main() {
     let region = Region {
@@ -14,54 +11,5 @@ fn main() {
             y: 0.0,
         },
         is_cluster: false,
-    };
-
-    let spec = r##"{
-        "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-        "data": {
-          "values": [
-            {
-              "x": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-              "y": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-            }
-          ]
-        },
-        "transform": [
-          {"flatten": ["x"]},
-          {"flatten": ["y"]},
-          {"calculate": "datum.x + ',' + datum.y", "as": "l"}
-        ],
-        "height": {"step": 32},
-        "width": {"step": 40},
-        "mark": {"type": "point", "size": 1600},
-        "encoding": {
-          "x": {"field": "x", "type": "ordinal", "axis": null},
-          "y": {"field": "y", "type": "ordinal", "axis": null},
-          "shape": {
-            "condition": {
-              "test": "datum.y % 2 == 1",
-              "value": "'M0,-0.6 L1,-1 L2,-0.6 L2,0.6 L1,1 L0,0.6 Z'"
-            },
-            "value": "'M-1,-0.6 L0,-1 L1,-0.6 L1,0.6 L0,1 L-1,0.6 Z'"
-          }
-        },
-        "config": {"view": {"stroke": null}}
-    }"##;
-
-    let builder = thread::Builder::new().stack_size(10 * 1024 * 1024); // 10MB stack size
-
-    let handler = builder.spawn(|| {
-        let chart: Result<Vegalite, serde_json::Error> = serde_json::from_str(spec);
-        match chart {
-            Ok(chart) => {
-                let _ = chart.show();
-            }
-            Err(err) => {
-                eprintln!("Error: {}", err);
-            }
-        }        
-    }).unwrap();
-
-    handler.join().unwrap();
-    
+    };    
 }
