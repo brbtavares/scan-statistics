c7d20733597912bcdfad315be3c32fc413fe48a0
brbtavares <brbtavares@outlook.com>
2024-04-09 01:15:43 -0300

    add `create_dist_matrix` function

--- src/definitions.rs
+++ src/definitions.rs
@@ -1,4 +1,5 @@
 use compute::distributions::*;
+
 pub struct Region {
     pub(crate) idx: usize,
     pub(crate) expectation: f64,
@@ -8,7 +9,13 @@ pub struct Region {
 }
 
 impl Region {
-    pub fn new(idx: usize, expectation: f64, cases: f64, centroid: Point, is_cluster: bool) -> Self {
+    pub fn new(
+        idx: usize,
+        expectation: f64,
+        cases: f64,
+        centroid: Point,
+        is_cluster: bool,
+    ) -> Self {
         Region {
             idx,
             expectation,
@@ -16,7 +23,11 @@ impl Region {
             centroid,
             is_cluster,
         }
-    }    
+    }
+
+    pub fn dist(&self, r: &Region) -> f64 {
+        ((self.centroid.x - r.centroid.x).powi(2) + (self.centroid.y - r.centroid.y).powi(2)).sqrt()
+    }
 }
 
 pub struct Point {
@@ -26,29 +37,103 @@ pub struct Point {
 
 impl Point {
     pub fn new(x: f64, y: f64) -> Self {
-        Point {
-            x,
-            y,
-        }
+        Point { x, y }
     }
-    
 }
 
-pub fn create_map() -> Vec<Region> {
+pub struct Matrix {
+    rows: usize,
+    cols: usize,
+    data: Vec<Vec<f64>>,
+}
+
+impl Matrix {
+    fn new(rows: usize, cols: usize) -> Self {
+        Matrix {
+            rows,
+            cols,
+            data: vec![vec![0.0; cols]; rows],
+        }
+    }
+
+    fn get(&self, row: usize, col: usize) -> Option<f64> {
+        if row < self.rows && col < self.cols {
+            Some(self.data[row][col])
+        } else {
+            None
+        }
+    }
+
+    fn set(&mut self, row: usize, col: usize, value: f64) -> bool {
+        if row < self.rows && col < self.cols {
+            self.data[row][col] = value;
+            true
+        } else {
+            false
+        }
+    }
+}
+
+pub fn create_map() -> (Vec<Region>, f64) {
     let centroid_jitter = Uniform::new(-0.5, 0.5);
     let expectation_gen = Uniform::new(0., 10.);
     let mut expectation_gen_sample: f64;
+    let mut expectation_total: f64 = 0.;
     let mut v: Vec<Region> = Vec::new();
     let mut k: usize = 0;
-    let cluster_indices: Vec<Vec<i32>> = vec![vec![5,7], vec![5,8], vec![6,6],vec![6,7], vec![6,8], vec![6,9], vec![7,5], vec![7,6], vec![7,7], vec![7,8], vec![7,9], vec![7,10], vec![8,6], vec![8,7], vec![8,8], vec![8,9], vec![9,7], vec![9,8]];
+    let cluster_indices: Vec<Vec<i32>> = vec![
+        vec![5, 7],
+        vec![5, 8],
+        vec![6, 6],
+        vec![6, 7],
+        vec![6, 8],
+        vec![6, 9],
+        vec![7, 5],
+        vec![7, 6],
+        vec![7, 7],
+        vec![7, 8],
+        vec![7, 9],
+        vec![7, 10],
+        vec![8, 6],
+        vec![8, 7],
+        vec![8, 8],
+        vec![8, 9],
+        vec![9, 7],
+        vec![9, 8],
+    ];
 
     for i in 1..14 {
         for j in 1..14 {
             expectation_gen_sample = expectation_gen.sample();
-            v.push(Region::new(k, expectation_gen_sample, Poisson::new(expectation_gen_sample).sample(), Point::new(f64::from(i)+centroid_jitter.sample(),f64::from(j)+centroid_jitter.sample()), if cluster_indices.contains(&vec![i,j])  {true} else {false}));
+            expectation_total += expectation_gen_sample;
+            v.push(Region::new(
+                k,
+                expectation_gen_sample,
+                Poisson::new(expectation_gen_sample).sample(),
+                Point::new(
+                    f64::from(i) + centroid_jitter.sample(),
+                    f64::from(j) + centroid_jitter.sample(),
+                ),
+                if cluster_indices.contains(&vec![i, j]) {
+                    true
+                } else {
+                    false
+                },
+            ));
             k = k + 1;
         }
     }
 
-    v
+    (v, expectation_total * 0.15)
+}
+
+pub fn create_dist_matrix(map: &[Region]) -> Matrix {
+    let l = map.len();
+    let mut m = Matrix::new(l, l);
+    for i in 0..l {
+        for j in 0..l {
+            m.set(i, j, map[i].dist(&map[j]));
+        }
+    }
+    m
 }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,6 @@ mod definitions;
 use crate::definitions::*;
 
 fn main() {
-
-    let map: Vec<Region> = create_map();
-    for v in map {
-        println!("The expectation of the region {} is {}, the number of cases is {} and its centroid is ({},{})...{}", v.idx, v.expectation, v.cases, v.centroid.x, v.centroid.y, v.is_cluster);
-    }
-    
-
-    
+    let (map, expectation_threshold) = create_map();
+    let dist_matrix = create_dist_matrix(&map);
 }
